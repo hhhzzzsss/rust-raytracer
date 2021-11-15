2ee3923e3c767c50f43799a1adf0ab09e7ac48d9
Athish Thiruvengadam <athisht2@illinois.edu>
2021-11-14 22:26:31 -0600

    Created Vec3D struct, Vec3D struct implementation and Color tuple struct

--- src/file_io.rs
+++ src/file_io.rs
@@ -1,3 +1,22 @@
+pub struct Vec3D {
+    x: u32,
+    y: u32,
+    z: u32
+}
+
+impl Vec3D {
+    pub fn new(x: u32, y: u32, z:u32) -> Vec3D {
+        let new_vec3d = Vec3D {
+            x,
+            y,
+            z
+        };
+        new_vec3d
+    }
+}
+
+struct Color(f32, f32, f32);
+
 pub mod file_io {
     pub fn test() {
         println!("Test function called!");
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+pub mod file_io;
+
 fn main() {
     println!("Hello, world!");
+
+    file_io::file_io::test();
 }

79ef91a75aca3642bf260c977c7ecee3ac4da03f
hhhzzzsss <hyzhou42@gmail.com>
2021-11-17 13:51:01 -0600

    Created hit_result.rs

---
+++ src/hit_result.rs
@@ -0,0 +1,19 @@
+pub struct HitResult {
+  position: Vec3D,
+	normal: Vec3D,
+}
+
+impl HitResult {
+  pub fn new(x: f64, y: f64, z:f64) -> Vec3D {
+    let new_vec3d = Vec3D {
+      x,
+      y,
+      z
+    };
+    new_vec3d
+  }
+
+  pub fn normalize(Vec3D vec) -> Vec3D {
+    todo!();
+  }
+}

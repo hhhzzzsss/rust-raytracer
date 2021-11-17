2a4223257fe16a67b32d9c0c4e006f1975791974
hhhzzzsss <hyzhou42@gmail.com>
2021-11-17 02:37:01 -0600

    Fixed indentation of vec3d.rs

--- src/vec3d.rs
+++ src/vec3d.rs
@@ -6,12 +6,12 @@ pub struct Vec3D {
 
 impl Vec3D {
   pub fn new(x: f64, y: f64, z:f64) -> Vec3D {
-      let new_vec3d = Vec3D {
-          x,
-          y,
-          z
-      };
-      new_vec3d
+    let new_vec3d = Vec3D {
+      x,
+      y,
+      z
+    };
+    new_vec3d
   }
 
   pub fn normalize(Vec3D vec) -> Vec3D {

2ab19e5af3dfea0ea03322b31cccf86c980213a5
hhhzzzsss <hyzhou42@gmail.com>
2021-11-18 11:21:13 -0600

    Added camera rotation

--- src/raytracing.rs
+++ src/raytracing.rs
@@ -1,14 +1,14 @@
-pub fn intersect() {
+pub fn intersect() -> Option<f64> {
   todo!();
 }
 
-pub fn trace() {
+pub fn trace() -> Vec3D {
   todo!();
 }
 
 pub fn render(Camera camera, Scene scene) -> Vec<<Vec<Vec3D>> {
-  // todo: implement camera rotation
   // todo: parallelism
+  Mat3D rotation_matrix = Mat3D::rotation(camera.pitch, camera.yaw, camera.roll);
   let image: Vec<Color> = Vec::new();
   for y in 0..camera.height {
     image.push(Vec::new());
@@ -20,7 +20,8 @@ pub fn render(Camera camera, Scene scene) -> Vec<<Vec<Vec3D>> {
       let field_ratio: f64 = camera.fov.tan() / 2;
       u *= field_ratio;
       v *= field_ratio;
-      image[x].push(trace(normalize(Vec3D {u, v, 1})));
+      Vec3D ray_direction = normalize(rotation_matrix * (Vec3D {u, v, 1}));
+      image[x].push(trace(camera.origin, ray_direction, scene));
     }
     image
   }

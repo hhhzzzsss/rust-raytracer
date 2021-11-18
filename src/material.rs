572302980be36855810712cb32886636f88a57af
hhhzzzsss <hyzhou42@gmail.com>
2021-11-18 14:58:46 -0600

    Minor change to material.rs

--- src/material.rs
+++ src/material.rs
@@ -1,11 +1,11 @@
 #[derive(Copy, Clone)]
 pub trait Material {
-    pub fn bounce(dir: Vec3D, nor: Vec3D, attenuation: &mut Vec3D, color: &mut Vec3D) -> Option<Vec3D>;
+    pub fn bounce(dir: Vec3D, hit_result: &HitResult, attenuation: &mut Vec3D, color: &mut Vec3D) -> Option<Vec3D>;
 }
 
 pub struct BlackHole;
 impl Material for BlackHole {
-    pub fn bounce(dir: Vec3D, nor: Vec3D, attenuation: &mut Vec3D, color: &mut Vec3D) -> Option<Vec3D> {
+    pub fn bounce(dir: Vec3D, hit_result: &HitResult, attenuation: &mut Vec3D, color: &mut Vec3D) -> Option<Vec3D> {
         return None();
     }
 }

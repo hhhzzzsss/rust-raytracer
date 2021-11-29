02b0b8eaa64a107fb4819738f6bf959f4b63c527
hhhzzzsss <hyzhou42@gmail.com>
2021-11-29 11:57:26 -0600

    Changed file_io to take Vec3D instead of Color

--- src/file_io.rs
+++ src/file_io.rs
@@ -1,14 +1,24 @@
 pub mod file_io {
     use image::{ImageBuffer, Rgb};
-    use crate::color::color::Color;
+    use crate::vec3d::Vec3D;
+    use crate::util::Clamp;
 
     pub fn test() {
         println!("Test function called!");
     }
 
+    fn Vec3D_to_Rgb(v: Vec3D) -> Rgb<u8> {
+        let rescaled = (v*256.).clamp(0., 255.);
+        Rgb([
+            v.x as u8,
+            v.y as u8,
+            v.z as u8
+        ])
+    }
+
     //Assumes that position in inner vector determines x-axis position
     //and position in outer vector determines y-axis position
-    pub fn vec_to_jpeg(colors: Vec<Vec<Color>>) {
+    pub fn vec_to_jpeg(colors: Vec<Vec<Vec3D>>) {
         let imgx: u32 = colors.get(0).unwrap().len() as u32;
         let imgy: u32 = colors.len() as u32;
 
@@ -16,8 +26,8 @@ pub mod file_io {
 
         for y in 0..imgy {
             for x in 0..imgx {
-                let color = colors.get(y as usize).unwrap().get(x as usize).unwrap();
-                img.put_pixel(x, y, Rgb([color.r, color.g, color.b]));
+                let color = *colors.get(y as usize).unwrap().get(x as usize).unwrap();
+                img.put_pixel(x, y, Vec3D_to_Rgb(color));
             }
         }
         img.save("image.jpg").unwrap();

8a4b5ac026b1fa035cec102a53f7b921d52b104f
hhhzzzsss <hyzhou42@gmail.com>
2021-11-30 14:44:53 -0600

    Implemented Sky

---
+++ src/sky.rs
@@ -0,0 +1,41 @@
+use crate::vec3d::Vec3D;
+use crate::util::mix;
+
+pub trait Sky {
+    fn get_color(&self, dir: Vec3D) -> Vec3D;
+}
+
+pub struct SolidSky {
+    color: Vec3D;
+}
+
+impl SolidSky {
+    pub fn new(color: Vec3D) -> Self {
+        return Self { color }
+    }
+}
+
+impl Sky for SolidSky {
+    fn get_color(&self, dir: Vec3D) -> Vec3D {
+        return self.color;
+    }
+}
+
+pub struct DomeGradientSky {
+    horizon_color: Vec3D,
+    zenith_color: Vec3D
+}
+
+impl DomeGradientSky {
+    const zenith_vec: Vec3D = Vec3D::new(0., 1., 0.);
+    pub fn new(horizon_color: Vec3D, zenith_color: Vec3D) -> Self {
+        return Self { horizon_color, zenith_color }
+    }
+}
+
+impl Sky for DomeGradientSky {
+    fn get_color(&self, dir: Vec3D) -> Vec3D {
+        let mut a = Vec3D::dot(dir, Self::zenith_vec).max(0.);
+        return mix(self.zenith_color, self.horizon_color, a);
+    }
+}
